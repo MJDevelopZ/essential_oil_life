f8cbbd55bdff542f27da970289ce30d360f10f30
Megan Zahr <megan.zahr@gmail.com>
2015-05-26 21:36:27 +0300

    Removed rouge coding displaying on pages

--- wp-content/themes/twentyfifteen-child/content-front_page.php
+++ wp-content/themes/twentyfifteen-child/content-front_page.php
@@ -3,11 +3,8 @@
  * The template used for displaying front-page content
  *
  * @package WordPress
-<<<<<<< HEAD
  * @subpackage Twenty_Fifteen_Child
-=======
  * @subpackage Twenty_Fifteen
->>>>>>> e20c142dd07548bfd84524916946a32b36f6b337
  * @since Twenty Fifteen 1.0
  */
 ?>
@@ -21,12 +18,8 @@
 	<div class="entry-content">
 		<?php the_content() ?>
 
-<<<<<<< HEAD
-		<a href="http://localhost:35555/home/" class="frontPageEnter">Enter</a>
-=======
-		<a href="http://localhost:51391/home/" class="frontPageEnter">Enter</a>
->>>>>>> e20c142dd07548bfd84524916946a32b36f6b337
-		
+		<a href="http://eolife.staging.wpengine.com/home/" class="frontPageEnter">Enter</a>
+	
 	</div><!-- .entry-content -->
 
 	
