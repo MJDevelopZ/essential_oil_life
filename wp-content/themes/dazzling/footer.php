0aa621efaee763f15753a732c710db36c899d2ec
Megan Zahr <megan.zahr@gmail.com>
2015-05-31 13:03:01 +0300

    Corrected erased footer.php to master theme file

--- wp-content/themes/dazzling/footer.php
+++ wp-content/themes/dazzling/footer.php
@@ -21,8 +21,8 @@
 					<?php dazzling_footer_links(); ?>
 				</nav>
 				<div class="copyright col-md-6">
-					<p> Theme by <a href="http://colorlib.com/wp/" target="_blank">Colorlib</a> Powered by <a href="http://wordpress.org/" target="_blank">WordPress</a> Customized by <a href="http://www.seraphinaknits.com" target="_blank">MJDevelopZ
-					</a></p>					
+					<?php echo of_get_option( 'custom_footer_text', 'dazzling' ); ?>
+					<?php dazzling_footer_info(); ?>					
 				</div>
 			</div><!-- .site-info -->
 			<div class="scroll-to-top"><i class="fa fa-angle-up"></i></div><!-- .scroll-to-top -->
