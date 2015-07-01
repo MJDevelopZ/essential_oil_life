9eb78ba8c921d8be9a8984a48577d9c153d81121
Megan Zahr <megan.zahr@gmail.com>
2015-07-01 23:44:57 +0300

    Removed slider widget, hard coded via template image grid with information link page in new window.

--- wp-content/themes/dazzling-child/content-front_page.php
+++ wp-content/themes/dazzling-child/content-front_page.php
@@ -5,13 +5,56 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 	<header class="entry-header page-header">
+		<h1 class="front-title"> <?php the_title(); ?> </h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php the_content(); ?>
-			
+		<ul class="monthly-specials">
+				
+				<?php 
+					$image_1 = get_field('image_1');
+					$image_1_link = get_field('image_1_link');
+					$image_2 = get_field('image_2');
+					$image_3 = get_field('image_3');
+					$image_4 = get_field('image_4');
+					$size = 'small';
+
+				?>
+
+				<li class="individual-monthly-specials" id="image-1"?>
+					<figure>
+						<?php if ($image_1 == true) { ?>
+							<?php echo '<a href="<?php echo $image_1_link; ?>" target="_blank">' . wp_get_attachment_image($image_1, $size) . '</a>' ;
+						} ?>
+					</figure>
+				</li>
+
+				<li class="individual-monthly-specials" id="image-2"?>
+					<figure>
+						<?php if ($image_2 == true) { ?>
+							<?php echo '<a href="<?php echo $image_2_link; ?>" target="_blank">' . wp_get_attachment_image($image_2, $size) . '</a>' ;
+						} ?>
+					</figure>
+				</li>
+
+				<li class="individual-monthly-specials" id="image-3"?>
+					<figure>
+						<?php if ($image_3 == true) { ?>
+							<?php echo '<a href="<?php echo $image_3_link; ?>" target="_blank">' . wp_get_attachment_image($image_3, $size) . '</a>' ;
+						} ?>
+					</figure>
+				</li>
+
+				<li class="individual-monthly-specials" id="image-4"?>
+					<figure>
+						<?php if ($image_4 == true) { ?>
+							<?php echo '<a href="<?php echo $image_4_link; ?>" target="_blank">' . wp_get_attachment_image($image_4, $size) . '</a>' ;
+						} ?>
+					</figure>
+				</li>
+		
+		</ul><!--.monthly-specials-->
 	</div><!-- .entry-content -->
-	
 </article><!-- #post-## -->
